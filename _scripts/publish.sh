455d862a6f60fe8cda0741329d34883e1f09bcaa
rodgers.r <rodgers.r@belgiumcampus.ac.za>
2019-03-17 17:54:01 +0200

    Config update.

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 bundle exec jekyll build
-cd _site
-rm -f site.zip
-zip -r site.zip .
-lftp -c "set dns:order \"inet\" ; open ftp://$FTP_HOST ; user $FTP_USER $FTP_PASSWORD ; put -e -O public_html site.zip ; exit"
-curl $UNPACK_URL
+lftp -c "set dns:order \"inet\" ; set ftp:ssl-allow no ; open ftp://$FTP_HOST ; user $FTP_USER $FTP_PASSWORD ; mirror -R _site $FTP_PATH ; exit"
--- _scripts/unpack.php
+++ _scripts/unpack.php
@@ -1,14 +1,37 @@
 <?php
-if (!file_exists('site.zip')) {
-  http_response_code(500);
-  echo 'Nothing to unpack';
-  exit();
+
+$new_name = 'archive/' . date('Y-m-d_G-i-s') . '.zip';
+if (!file_exists('archive')) { mkdir('archive'); }
+
+if (file_exists('site.zip')) {
+
+  rename('site.zip', $new_name);
+
+} else {
+
+  if ($_GET['key'] !== file_get_contents('secret.key')) {
+    http_response_code(500);
+    echo 'Key mismatch';
+    exit();
+  }
+
+  if (!isset($_FILES['sitefile']['error']) ||
+      is_array($_FILES['sitefile']['error']) ||
+      $_FILES['sitefile']['error'] != UPLOAD_ERR_OK) {
+    http_response_code(500);
+    echo 'Nothing to unpack';
+    exit();
+  }
+
+  $tmp_name = $_FILES['sitefile']['tmp_name'];
+  move_uploaded_file($tmp_name, $new_name);
+
 }
+
 $zip = new ZipArchive;
-if ($zip->open('site.zip') === TRUE) {
+if ($zip->open($new_name) === TRUE) {
   $zip->extractTo('.');
   $zip->close();
-  unlink('site.zip');
   echo 'Unpacked OK';
   exit();
 } else {
@@ -16,4 +39,5 @@ if ($zip->open('site.zip') === TRUE) {
   echo 'Failed';
   exit();
 }
+
 ?>
